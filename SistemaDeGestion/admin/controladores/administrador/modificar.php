34bc9b9ec37b68d14a1eba6d9fa59a06beab8daf
JannethE-Matute <84471343+JannethE-Matute@users.noreply.github.com>
2021-05-25 22:43:54 -0500

    Create modificar.php
    Creación del php de modificar

---
+++ SistemaDeGestion/admin/controladores/administrador/modificar.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Modificar datos de perona</title>
+    </head>
+    <body>
+    <?php
+   
+   session_start();
+ 
+   if (!isset($_SESSION['rol'])) {
+       # code... 
+       header( "Location:/SistemaDeGestion/public/controladores/login.php");
+   }else {
+       # code...
+       if ($_SESSION['rol'] ==1) {
+ 
+           header("Location:/SistemaDeGestion/admin/vista/administrador/accesoda.php");
+           //echo "<p>no es administrador</p>";
+       }
+   }
+   ?>
+        <?php
+        include "../../../config/conexionBD.php";
+        $codigoue = $_POST["codigoue"];
+        $codigo = $_POST["codigo"];
+        $cedula = isset($_POST["cedula"])?trim($_POST["cedula"]):null;
+        $nombres = isset($_POST["nombres"])?mb_strtoupper(trim($_POST["nombres"]),'UTF-8'):null;
+        $apellidos = isset($_POST["apellidos"])?mb_strtoupper(trim($_POST["apellidos"]),'UTF-8'):null;
+        $direccion = isset($_POST["direccion"])?mb_strtoupper(trim($_POST["direccion"]),'UTF-8'):null;
+        $telefono = isset($_POST["telefono"])?trim($_POST["telefono"]):null;
+        $correo = isset($_POST["correo"])?trim($_POST["correo"]):null;
+        $fechaNacimiento = isset($_POST["fechaNacimiento"])?trim($_POST["fechaNacimiento"]):null;
+       
+        date_default_timezone_set("America/Guayaquil");
+        $fecha=date('Y-m-d H:i:s',time());
+
+        $sql="UPDATE usuario SET" .
+            " usu_cedula = '$cedula',".
+            "usu_nombres = '$nombres',".
+            "usu_apellidos = '$apellidos',".
+            "usu_direccion = '$direccion',".
+            "usu_telefono = '$telefono',".
+            "usu_correo = '$correo',".
+            "usu_fecha_nacimiento = '$fechaNacimiento',".
+            "usu_fecha_modificacion = '$fecha'".
+            "WHERE usu_codigo = '$codigo'";
+        
+            
+        if ($conn->query($sql)=== TRUE) {
+                # code...
+                header("Location:../../vista/administrador/inicioa.php?codigoU=".$codigoue."");
+        } else{
+                echo "<p> Error:".$sql."<br>".mysqli_error($conn)."</p>";
+        }
+           
+    
+
+        $conn->close();
+
+        ?>
+
+    </body>
+</html>
